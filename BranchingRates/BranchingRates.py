43f6c0cb69e68b7469a31d98395715bf56a9ff78
Arturo AA <arturo.arellano@cimat.mx>
2025-02-12 20:47:44 -0500

    failed tests

--- BranchingRates/BranchingRates.py
+++ BranchingRates/BranchingRates.py
@@ -10,7 +10,7 @@ def g_2(x,y):
   xf=x-math.floor(x)
   yf=y-math.floor(y)
   if(xf==0):
-    return 1
+    return 0
   m=yf/xf
   if xf==0:
     return 1
@@ -22,4 +22,4 @@ def g_2(x,y):
     return 1
   
 def g_3(x,y):
-  return 3*np.sin(x*2*(np.pi))*np.sin(y*2*(np.pi))+3
+  return np.cos(2*x*np.pi)+2
