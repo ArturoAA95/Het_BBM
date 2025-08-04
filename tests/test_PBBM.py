dcd4f8c265c6727044ec6fc3fef1ea7f807b3225
ArturoAA95 <120545077+ArturoAA95@users.noreply.github.com>
2025-08-04 15:05:20 -0400

    plotting BBM change

--- tests/test_PBBM.py
+++ tests/test_PBBM.py
@@ -3,13 +3,18 @@ from BBM import Pg_BBM
 from BBM import PlotBBM
 from BBM import PlotBBM_CH
 from BranchingRates import g_2
+from BranchingRates import g_0
+from BBM import g_BBM_killing
+import numpy as np
 
-T = 22
-N = 18
+T = 60
+wd = int(T*np.sqrt(2)) + 5
+N = 12
 eps = 10**(-15)
-A = Pg_BBM([], T, N, eps, g_2)
+#A = Pg_BBM([], T, N, eps, g_0)
+A = g_BBM_killing([], T, N, eps, g_0)
 for i in range(N):
-    PlotBBM_CH(A[i], str(i), 26)
+    PlotBBM_CH(A[i], str(i), wd, (i+1)*(T/N))
 
 #for i in range(N):
 #    PlotBBM(A[i])
