376fb3a9cf906d834f78fa97efb4ba8849fad036
ArturoAA95 <120545077+ArturoAA95@users.noreply.github.com>
2025-08-04 15:04:32 -0400

    changes in test of the branching rate

--- tests/test_BR.py
+++ tests/test_BR.py
@@ -5,19 +5,20 @@ from BranchingRates import g_2
 
 # Define the window for the heatmap
 x_min = 0
-x_max = 2
+x_max = 10
 y_min = 0
-y_max = 2
+y_max = 10
+m = 1000
 
 # Create a grid of x and y values within the window
-x = np.linspace(x_min, x_max, 100)
-y = np.linspace(y_min, y_max, 100)
+x = np.linspace(x_min, x_max, m)
+y = np.linspace(y_min, y_max, m)
 X, Y = np.meshgrid(x, y)
 
 # Calculate the function values for each point in the grid
-Z = np.empty((100,100))
-for i in range(100):
-  for j in range(100):
+Z = np.empty((m,m))
+for i in range(m):
+  for j in range(m):
     Z[i,j] = g_2(X[i,j],Y[i,j])
 
 # Create the heatmap
